1a262505747018a09b64e1718a8e80aeddee59b1
Arnau Sanchez <pyarnau@gmail.com>
2024-06-12 12:56:06 +0200

    poc: Use approval app as plugin

--- src/pages/data-approval/DataApprovalTable.tsx
+++ src/pages/data-approval/DataApprovalTable.tsx
@@ -1,17 +1,9 @@
 import React from "react";
 import { LinearProgress } from "@material-ui/core";
 // @ts-ignore
-import { CssVariables } from "@dhis2/ui";
-// @ts-ignore
-import { SelectionProvider } from "approval-app/es/selection-context";
-// @ts-ignore
-import { AppProvider } from "approval-app/es/app-context";
-// @ts-ignore
-import { Layout } from "approval-app/es/app/layout.js";
-// @ts-ignore
-import { Display } from "approval-app/es/data-workspace/display";
-// @ts-ignore
-import { useSelectionContext } from "approval-app/es/selection-context";
+import postRobot from "@krakenjs/post-robot";
+import { useBooleanState } from "../../components/utils/use-boolean";
+import { useAppContext } from "../../contexts/api-context";
 
 export interface DataApprovalTableProps {
     dataSetId: string;
@@ -20,41 +12,83 @@ export interface DataApprovalTableProps {
     attributeOptionComboId: string;
 }
 
-const DataApprovalTable: React.FC<DataApprovalTableProps> = props => {
+export function useDhis2Url(path: string) {
+    const { api, isDev } = useAppContext();
+    return (isDev ? "/dhis2" : api.baseUrl) + path;
+}
+
+export const DataApprovalTable: React.FunctionComponent<DataApprovalTableProps> = props => {
+    const pluginUrl = useDhis2Url("/dhis-web-approval/plugin.html");
+    const iframeRef = React.useRef<HTMLIFrameElement>(null);
+    const isPluginReady = useDataApprovalPlugin(iframeRef, props);
+
     return (
         <>
-            <CssVariables spacers colors theme />
+            {!isPluginReady && <LinearProgress />}
 
-            <AppProvider>
-                <SelectionProvider disableHistory={true}>
-                    <DataValuesTableContents {...props} />
-                </SelectionProvider>
-            </AppProvider>
+            <iframe
+                src={pluginUrl}
+                ref={iframeRef}
+                style={styles.iframe}
+                width={"100%"}
+                height={500}
+            />
         </>
     );
 };
 
-const DataValuesTableContents: React.FC<DataApprovalTableProps> = props => {
-    const { orgUnitId, dataSetId, period, attributeOptionComboId } = props;
-    const selection = useSelectionContext();
-    const isSelectionFilled = selection.orgUnit && selection.period;
+const styles = {
+    iframe: { border: "none", overflow: "hidden" },
+};
+
+type Size = {
+    width: number;
+    height: number;
+};
+
+function useDataApprovalPlugin(
+    iframeRef: React.RefObject<HTMLIFrameElement>,
+    dataApproval: object
+) {
+    React.useEffect(() => {
+        const iframe = iframeRef.current;
+        if (!iframe || !dataApproval) return;
+
+        const pluginProps = {
+            dataApproval: dataApproval,
+        };
+
+        postRobot.send(iframe.contentWindow, "newProps", pluginProps);
+
+        const listener = postRobot.on(
+            "getProps",
+            { window: iframeRef.current.contentWindow },
+            () => pluginProps
+        );
+
+        return () => listener.cancel();
+    }, [iframeRef, dataApproval]);
+
+    const [isPluginReady, { enable: setPluginAsReady }] = useBooleanState(false);
 
     React.useEffect(() => {
-        if (isSelectionFilled) return;
-        selection.selectOrgUnit({ id: orgUnitId });
-        selection.selectPeriod(period);
-        selection.selectFilter(`ao:${attributeOptionComboId}`);
-    }, [selection, isSelectionFilled, orgUnitId, period, attributeOptionComboId]);
+        if (!iframeRef?.current) return;
 
-    if (!isSelectionFilled) return <LinearProgress />;
+        const listener = postRobot.on(
+            "installationStatus",
+            { window: iframeRef.current.contentWindow },
+            (ev: { data: { installationStatus: "READY" | "INSTALLING" | "UNKNOWN" } }) => {
+                // await navigator.serviceWorker.getRegistration() return undefined, no PWA for capture-app
+                //if (ev.data.installationStatus === "READY") {
+                setPluginAsReady();
+                //}
+            }
+        );
 
-    return (
-        <Layout.Container>
-            <Layout.Content>
-                <Display dataSetId={dataSetId} />
-            </Layout.Content>
-        </Layout.Container>
-    );
-};
+        return () => listener.cancel();
+    }, [iframeRef, setPluginAsReady]);
+
+    return isPluginReady;
+}
 
 export default React.memo(DataApprovalTable);
